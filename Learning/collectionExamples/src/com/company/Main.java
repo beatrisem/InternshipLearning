306c6cbb6c1cb13d679a8afd96b0bcfdfa7da654
beatrisem <49399314+beatrisem@users.noreply.github.com>
2020-09-01 17:35:55 +0300

    Add files via upload

--- Learning/collectionExamples/src/com/company/Main.java
+++ Learning/collectionExamples/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //ArrayList example
+
+        List<String> names = new ArrayList<>();
+        names.add("Jānis");
+        names.add("Pēteris");
+        names.add("Anna");
+        System.out.println(names);
+
+        //LinkedList example
+        LinkedList<String> animals = new LinkedList<>();
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("cow");
+        System.out.println("Animals: "+animals);
+
+        LinkedList<String> birds = new LinkedList<>();
+        birds.add("swan");
+        birds.add("crow");
+
+        //add one linked list to another
+        animals.addAll(birds);
+        System.out.println("Animals and birds:"+ animals);
+
+        //listIterator
+
+        ListIterator<String> listIterate = animals.listIterator();
+        System.out.println("List iterator:");
+
+        while (listIterate.hasNext()){ //also can use hasPrevious()
+            System.out.println(listIterate.next());
+            System.out.println(", ");
+        }
     }
 }
